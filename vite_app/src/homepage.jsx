db89e38c3b4343b6ac75873a7ad737fd59b46f9f
krishna <krishnaparidwal9879@gmail.com>
2024-12-04 14:20:54 +0530

    first vite app

---
+++ vite_app/src/homepage.jsx
@@ -0,0 +1,194 @@
+import './boat.css';
+
+function Newton() {
+  return (
+    <>
+      <div className="grey">
+        <div className="t1">
+          Get 5% off on First Order | Code: WELCOME% | Shop Now!
+        </div>
+      </div>
+
+      <nav className="nav">
+        <img
+          src="https://www.boat-lifestyle.com/cdn/shop/files/boAt_logo_small_3067da8c-a83b-46dd-b28b-6ef1e16ccd17_1.svg?v=1732879309"
+          className="boat"
+          alt="boAt Logo"
+        />
+        <div className="search"></div>
+        <div className="cate">Categories</div>
+        <div className="person">boAt Personalisation</div>
+        <div className="corp">Corporate Orders</div>
+        <div className="more">More</div>
+        <div className="t2">Search "Earphones"</div>
+        <img
+          src="https://cdn.shopify.com/s/files/1/0057/8938/4802/files/Profile-1.png?v=1663762989"
+          alt="User"
+          className="user"
+        />
+      </nav>
+
+      <i className="fa-sharp fa-solid fa-magnifying-glass"></i>
+
+      <img
+        src="https://www.boat-lifestyle.com/cdn/shop/files/ION_Banner_WEB_f2f301b9-04e1-41f9-b424-a024680e6acc_1400x.jpg?v=1727264228"
+        className="image1"
+        alt="ION Banner"
+      />
+
+      <div className="bar2">
+        <img
+          src="https://www.boat-lifestyle.com/cdn/shop/files/Group_334305_small.svg?v=1682336123"
+          className="warrentyi"
+          alt="1 Year Warranty Icon"
+        />
+        <div className="warrentyt">
+          1 Year <br /> Warranty
+        </div>
+
+        <img
+          src="https://www.boat-lifestyle.com/cdn/shop/files/Group_334304_small.svg?v=1682336123"
+          className="replacei"
+          alt="7-Day Replacement Icon"
+        />
+        <div className="replacet">
+          7-Day <br /> Replacement
+        </div>
+
+        <img
+          src="https://www.boat-lifestyle.com/cdn/shop/files/Group_334303_small.svg?v=1682336123"
+          className="deliveryi"
+          alt="Free Express Delivery Icon"
+        />
+        <div className="deliveryt">
+          Free Express <br /> Delivery*
+        </div>
+
+        <img
+          src="https://www.boat-lifestyle.com/cdn/shop/files/Group_334302_small.svg?v=1682336123"
+          className="gsti"
+          alt="GST Billing Icon"
+        />
+        <div className="gstt">
+          GST <br /> Billing
+        </div>
+      </div>
+
+      <div className="text4">
+        Sale <span className="live">Is Live</span>
+      </div>
+
+      <img
+        src="https://www.boat-lifestyle.com/cdn/shop/files/Strip_Desktop1-3_aff6fba8-0f08-486d-b1fc-49b4585a0ec9_1400x.png?v=1732906802"
+        className="pinkimg"
+        alt="Sale Banner"
+      />
+
+      <div className="main">
+        {/* Card 1: boAt Airdopes 71 */}
+        <div className="body71">
+          <div className="yellow71">
+            <img
+              src="https://cdn.shopify.com/s/files/1/0057/8938/4802/files/71.png?v=1732859921"
+              className="img71"
+              alt="boAt Airdopes 71"
+            />
+          </div>
+          <div className="text5">boAt Airdopes 71</div>
+          <div className="dotted">------------------------------------</div>
+          <div className="price">₹999</div>
+          <div className="circle1" style={{ backgroundColor: "rgb(0, 4, 11)" }}></div>
+          <div className="circle2" style={{ backgroundColor: "rgb(140, 176, 243)" }}></div>
+        </div>
+
+        {/* Card 2: boAt Airdopes 280 ANC */}
+        <div className="body71">
+          <div className="yellow71">
+            <img
+              src="https://cdn.shopify.com/s/files/1/0057/8938/4802/files/280_lr.png?v=1721292483"
+              className="img71"
+              alt="boAt Airdopes 280 ANC"
+            />
+          </div>
+          <div className="text5">boAt Airdopes 280 ANC</div>
+          <div className="dotted">------------------------------------</div>
+          <div className="price">₹1,399</div>
+          <div className="circle1" style={{ backgroundColor: "rgb(0, 4, 11)" }}></div>
+          <div className="circle2" style={{ backgroundColor: "rgb(140, 176, 243)" }}></div>
+        </div>
+
+        {/* Card 3: boAt Airdopes 800 */}
+        <div className="body71">
+          <div className="yellow71">
+            <img
+              src="https://cdn.shopify.com/s/files/1/0057/8938/4802/files/Airdop800_Black.png?v=1716051692"
+              className="img71"
+              alt="boAt Airdopes 800"
+            />
+          </div>
+          <div className="text5">boAt Airdopes 800</div>
+          <div className="dotted">------------------------------------</div>
+          <div className="price">₹1,599</div>
+          <div className="circle1" style={{ backgroundColor: "rgb(0, 3, 8)" }}></div>
+          <div className="circle2" style={{ backgroundColor: "rgb(217, 218, 219)" }}></div>
+        </div>
+
+        {/* Card 4: boAt Airdopes 131 */}
+        <div className="body71">
+          <div className="yellow71">
+            <img
+              src="https://cdn.shopify.com/s/files/1/0057/8938/4802/files/131_f04f74fd-45d4-4614-85cf-6ccf69c4cf90.jpg?v=1691395049"
+              className="img71"
+              alt="boAt Airdopes 131"
+            />
+          </div>
+          <div className="text5">boAt Airdopes 131</div>
+          <div className="dotted" style={{ color: "rgb(188, 187, 187)" }}>
+            ------------------------------------
+          </div>
+          <div className="price">₹899</div>
+          <div className="circle1" style={{ backgroundColor: "rgb(0, 0, 0)" }}></div>
+          <div className="circle2" style={{ backgroundColor: "rgb(164, 165, 167)" }}></div>
+        </div>
+
+        {/* Card 5: boAt Airdopes 181 Pro */}
+        <div className="body71">
+          <div className="yellow71">
+            <img
+              src="https://cdn.shopify.com/s/files/1/0057/8938/4802/files/Lifestyle.jpg?v=1717817159"
+              className="img71"
+              alt="boAt Airdopes 181 Pro"
+            />
+          </div>
+          <div className="text5">boAt Airdopes 181 Pro</div>
+          <div className="dotted" style={{ color: "rgb(188, 187, 187)" }}>
+            ------------------------------------
+          </div>
+          <div className="price">₹1,199</div>
+          <div className="circle1" style={{ backgroundColor: "rgb(29, 33, 20)" }}></div>
+          <div className="circle2" style={{ backgroundColor: "rgb(140, 176, 243)" }}></div>
+        </div>
+
+        {/* Card 6: boAt Airdopes 161 */}
+        <div className="body71">
+          <div className="yellow71">
+            <img
+              src="https://cdn.shopify.com/s/files/1/0057/8938/4802/files/AD_161.jpg?v=1686297917"
+              className="img71"
+              alt="boAt Airdopes 161"
+            />
+          </div>
+          <div className="text5">boAt Airdopes 161</div>
+          <div className="dotted" style={{ color: "rgb(188, 187, 187)" }}>
+            ------------------------------------
+          </div>
+          <div className="price">₹999</div>
+          <div className="circle1" style={{ backgroundColor: "rgb(0, 0, 0)" }}></div>
+          <div className="circle2" style={{ backgroundColor: "rgb(67, 59, 107)" }}></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Newton;
